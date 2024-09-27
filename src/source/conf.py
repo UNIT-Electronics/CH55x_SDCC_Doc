e6ee2a624fce890e4f837bb5173e2acb80c83499
Cesarbautista10 <devuging@gmail.com>
2024-09-27 15:48:17 -0600

    chore: add config

--- src/source/conf.py
+++ src/source/conf.py
@@ -21,11 +21,14 @@ extensions = [
 ]
 
 templates_path = ['_templates']
+html_theme_options = {
+    "theme_switcher": False,  # Desactiva el interruptor de cambio de tema
+}
 exclude_patterns = []
 master_doc = 'index'
 numfig = True
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 html_logo = "_static/Logo-UNIT_Web-04-800x182.png"
 html_static_path = ['_static']
 latex_logo = "_static/Logo-UNIT_Web-04-800x182.png"
